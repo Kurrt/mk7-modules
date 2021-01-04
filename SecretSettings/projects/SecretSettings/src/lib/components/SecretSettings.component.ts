c5e8ee9ba80367b73fb5dad78728b9dd4ec6a258
Kurrt <dev@kurrt.com>
2021-01-04 19:25:58 +1100

    more options UI only

--- SecretSettings/projects/SecretSettings/src/lib/components/SecretSettings.component.ts
+++ SecretSettings/projects/SecretSettings/src/lib/components/SecretSettings.component.ts
@@ -16,11 +16,27 @@ export class SecretSettingsComponent implements OnInit {
     constructor(private API: ApiService) { }
 
     securityTypes: SecurityType[] = [
-        {value: 'none', viewValue: 'None'},
-        {value: 'wep', viewValue: 'WEP'},
-        {value: 'wpa', viewValue: 'WPA'},
-        {value: 'wpa2', viewValue: 'WPA2'},
-        {value: 'wpa3', viewValue: 'WPA3'}
+        {value: 'none', viewValue: 'None - RECOMMENDED'},
+        {value: 'psk2+tkip+ccmp', viewValue: 'WPA2 Personal (psk2+tkip+ccmp)'},
+        {value: 'psk2+tkip+aes', viewValue: 'WPA2 Personal (psk2+tkip+aes)'},
+        {value: 'psk2+tkip', viewValue: 'WPA2 Personal (psk2+tkip)'},
+        {value: 'psk2+ccmp', viewValue: 'WPA2 Personal (psk2+ccmp) - RECOMMENDED'},
+        {value: 'psk2+aes', viewValue: 'WPA2 Personal (psk2+aes)'},
+        {value: 'psk2', viewValue: 'WPA2 Personal (psk2)'},
+        {value: 'psk+tkip+ccmp', viewValue: 'WPA Personal (psk+tkip+ccmp)'},
+        {value: 'psk+tkip+aes', viewValue: 'WPA Personal (psk+tkip+aes)'},
+        {value: 'psk+tkip', viewValue: 'WPA Personal (psk+tkip)'},
+        {value: 'psk+ccmp', viewValue: 'WPA Personal (psk+ccmp)'},
+        {value: 'psk+aes', viewValue: 'WPA Personal (psk+aes)'},
+        {value: 'psk', viewValue: 'WPA Personal (psk)'},
+        {value: 'psk-mixed+tkip+ccmp', viewValue: 'WPA/WPA2 Personal (psk-mixed+tkip+ccmp)'},
+        {value: 'psk-mixed+tkip+aes', viewValue: 'WPA/WPA2 Personal (psk-mixed+tkip+aes)'},
+        {value: 'psk-mixed+tkip', viewValue: 'WPA/WPA2 Personal (psk-mixed+tkip)'},
+        {value: 'psk-mixed+ccmp', viewValue: 'WPA/WPA2 Personal (psk-mixed+ccmp)'},
+        {value: 'psk-mixed+aes', viewValue: 'WPA/WPA2 Personal (psk-mixed+aes)'},
+        {value: 'psk-mixed', viewValue: 'WPA/WPA2 Personal (psk-mixed)'},
+        {value: 'wep+open', viewValue: 'WEP Open'},
+        {value: 'wep+shared', viewValue: 'WEP Shared'}
     ];
 
     interfaces: string[] = [
